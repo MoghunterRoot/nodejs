9065d424da674f0a8d462e7f93c710cc6992f44d
vviannay <vincent.viannay@agilium.com>
2019-03-08 17:25:26 +0100

    connection a la base de donnée dans l'api

--- nodejs_tuto/mysql-demo/app.js
+++ nodejs_tuto/mysql-demo/app.js
@@ -4,14 +4,26 @@ var db = mysql.createConnection({
 	host: "localhost",
 	database: "nodejs",
 	user: "root",
-	password: "po"
+	password: ""
 
 })
 
 db.connect((err) => {
 	if(err)
 		console.log(err.message)	
-	
 	else 
-		console.log("Successfuly Connected to database")	
+		console.log("===== CONNECTION =====\n")
+		console.log(' Connected to database on localhost:3306\n')
+
+		db.query("SELECT * from members", (err, result) => {
+			if (err){
+				console.log(err.message)
+			}
+			else {
+				console.log("====== SQL QUERY =======\n")
+				console.log(result)
+				console.log("The Result of Query is " + "["+result[2].name+"]") 	
+			}
+		})
+		
 })
